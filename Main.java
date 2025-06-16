c404caaa845a58a269917e0fff21141a934bae8e
unknown <m1m02530121@oit.ac.jp>
2025-06-16 11:35:59 +0900

    Main.java修正 (v1.1用)

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SEMonster version 1.1!");
+        Player player = new Player();
+        player.drawMonsters();
+        player.showDeck();
+    }
+}
